932f3ced51e61ea2cb0e18fa12d3a0739f588991
Gonzalo <gbd1004@alu.ubu.es>
2022-09-21 19:31:44 +0200

    Quitar cosas

--- LibClass/Usuario.cs
+++ LibClass/Usuario.cs
@@ -6,42 +6,10 @@ namespace LibClass
     {
         private int idUsuario;
         private string nombre;
-        private string apellidos;
-        private string email;
+        private string rol;
         private string contraseña_encriptada;
 
-        public Usuario(int idUsuario, string nombre, string apellidos, string email, string contraseña_encriptada)
-        {
-            this.idUsuario = idUsuario;
-            this.nombre = nombre;
-            this.apellidos = apellidos;
-            this.email = email;
-            this.contraseña_encriptada = contraseña_encriptada;
-        }
-
-        public int IdUsuario
-        {
-            get { return idUsuario; }
-            set { idUsuario = value; }
-        }
-
-        public string Nombre
-        {
-            get { return nombre; }
-            set { nombre = value; }
-        }
-
-        public String Apellidos
-        {
-            get { return apellidos; }
-            set { apellidos = value; }
-        }
-
-        public String Email
-        {
-            get { return email; }
-            set { email = value; }
-        }
+        
 
     }
 }
