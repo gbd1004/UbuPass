f69404c40c7e131553f1b382680d1a6951f2ee6a
gonza <gonza@GONZALO>
2022-09-21 19:22:45 +0200

    Agregar archivos de proyecto.

---
+++ LibClass/Usuario.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibClass
+{
+    public class Usuario
+    {
+        private int idUsuario;
+        private string nombre;
+        private string apellidos;
+        private string email;
+        private string contraseña_encriptada;
+
+        public Usuario(int idUsuario, string nombre, string apellidos, string email, string contraseña_encriptada)
+        {
+            this.idUsuario = idUsuario;
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.email = email;
+            this.contraseña_encriptada = contraseña_encriptada;
+        }
+
+        public int IdUsuario
+        {
+            get { return idUsuario; }
+            set { idUsuario = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public String Apellidos
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+
+        public String Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+    }
+}
---
+++ LibClassTests/UsuarioTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibClass.Tests
+{
+    [TestClass()]
+    public class UsuarioTests
+    {
+        [TestMethod()]
+        public void UsuarioTest()
+        {
+            Usuario u = new Usuario(1, "Pedro", "Jaja", "sisisisisis@pito.cum", "1234");
+
+            Assert.IsNotNull(u);
+            Assert.AreEqual(u.Nombre, "Pedro");
+            u.Nombre = "Peter";
+            Assert.AreEqual(u.Nombre, "Peter");
+            Assert.IsFalse(u.Nombre.Equals("Pedro"));
+        }
+    }
+}
