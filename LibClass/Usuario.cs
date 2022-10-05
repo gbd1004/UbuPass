5d9de31ec6bf79877603e7cd638930b106533350
Gonzalo <gbd1004@alu.ubu.es>
2022-10-05 17:24:37 +0200

    Email

--- LibClass/Usuario.cs
+++ LibClass/Usuario.cs
@@ -4,13 +4,15 @@
     {
         private readonly int idUsuario;
         private string nombre;
+        private string email;
         private bool esGestor;
         private string contraseñaEncriptada;
 
-        public Usuario(int idUsuario, string nombre, bool esGestor, string contraseñaEncriptada)
+        public Usuario(int idUsuario, string nombre, string email, bool esGestor, string contraseñaEncriptada)
         {
             this.idUsuario = idUsuario;
             this.nombre = nombre;
+            this.email = email;
             this.esGestor = esGestor;
             this.contraseñaEncriptada = contraseñaEncriptada;
         }
@@ -19,5 +21,6 @@
         public string Nombre { get => Nombre1; set => Nombre1 = value; }
         public string Nombre1 { get => nombre; set => nombre = value; }
         public string ContraseñaEncriptada { set => contraseñaEncriptada = value; }
+        public string Email { get => email; set => email = value; }
     }
 }
